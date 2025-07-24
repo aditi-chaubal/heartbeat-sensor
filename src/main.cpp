b081e4dae723d0bfa0d6fc7f240a176923e3cbd5
Aditi Chaubal <aditichaubal@Aditis-MacBook-Air-2.local>
2025-07-23 23:37:40 -0500

    detect heartbeat working

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,33 @@
 #include <Arduino.h>
+#include "pulseSensor.cpp"
 
 int const pulse_sensor_pin = 0;
-int signal;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
  
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  signal = analogRead(pulse_sensor_pin);
-  float time = millis();
-  Serial.print(time);
-  Serial.print(",");
-  Serial.println(signal);
+pulse newPulse;
+
+/*
+void loop() 
+{
+  int signal = analogRead(pulse_sensor_pin);
+  if (newPulse.detectBeat(signal) == true)
+  {
+    Serial.println (newPulse.calculateBPM());
+  }
+  
   delay(10);
+  
+}
+*/
+
+void loop()
+{
+  int signal = analogRead(pulse_sensor_pin);
+  newPulse.detectBeat(signal);
 }
--- src/pulseSensor.cpp
+++ src/pulseSensor.cpp
@@ -1,19 +1,43 @@
-extern int signal;
-extern const int pulse_sensor_pin;
-
+#include <Arduino.h>
 
 class pulse
 {
 public:
-    int threshold;
 
-    bool foundBeat()
+    int threshold;
+    int counter = 0;
+    long lastTime = 0;
+    long currentTime = 0;
+
+    bool detectBeat(int signal) 
     {
-        bool status = true;
+        bool status = false;
         if (signal == 550)
         {
-            status = true;
+            counter = counter + 1;
+            if (counter % 2 == 0)
+            {
+                status = true;
+                lastTime = currentTime;
+                currentTime = millis();
+            }
+            Serial.println("signal: " + String(signal) + " Heartbeat detected"); 
         }
         return status;
     }
+
+    int calculateBPM()
+    {
+        if (lastTime == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            long timeBetween = currentTime - lastTime;
+            int bpm = 60000 / timeBetween;
+            return bpm;
+        } 
+    }
+    
 };
